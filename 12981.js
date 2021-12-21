1fdafadf6b1e5604eec93e5fcc33e226b3551608
Minkyu Kim <kig0421@gmail.com>
2021-12-21 23:04:34 +0900

    Create 12981.js

---
+++ 12981.js
@@ -0,0 +1,26 @@
+// 영어 끝말잇기
+function solution(n, words) {
+  let lastCaracter = "";
+  const targetWords = [...words];
+  const useKeywords = [];
+
+  while (targetWords.length) {
+    const shiftKeyword = targetWords.shift();
+    if (useKeywords.find((i) => i === shiftKeyword)) {
+      break;
+    }
+
+    if (lastCaracter === "" || lastCaracter === shiftKeyword[0]) {
+      useKeywords.push(shiftKeyword);
+      lastCaracter = shiftKeyword[shiftKeyword.length - 1];
+    } else {
+      break;
+    }
+  }
+
+  if (useKeywords.length === words.length) {
+    return [0, 0];
+  } else {
+    return [(useKeywords.length % n) + 1, Math.ceil((useKeywords.length + 1) / n)];
+  }
+}
