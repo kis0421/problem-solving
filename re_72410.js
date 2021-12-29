9ca5a5a3e86764927d4adf34499f248ef58a4615
Minkyu Kim <kig0421@gmail.com>
2021-12-29 21:55:03 +0900

    Create re_72410.js

---
+++ re_72410.js
@@ -0,0 +1,17 @@
+// 신규 아이디 추천
+
+function solution(new_id) {
+  let newId = new_id.toLowerCase()
+  .replace(/[^\.a-z0-9-_]/g, '')
+  .replace(/\.+/g, '.')
+  .replace(/^\.|\.+$/, '');
+    
+  if (newId === "") {
+    newId = "a";
+  }
+  newId = newId.substring(0, 15).replace(/\.+$/, '');
+  while (newId.length < 3) {
+    newId = `${newId}${newId[newId.length - 1]}`;
+  }
+  return newId;
+}
