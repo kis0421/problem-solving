a06b99d88639c79c52904928452fd4a3e7d5aa49
Minkyu Kim <kig0421@gmail.com>
2022-01-30 19:12:43 +0900

    Create 42583.js

---
+++ 42583.js
@@ -0,0 +1,24 @@
+// 다리를 지나는 트럭
+
+function solution(bridge_length, weight, truck_weights) {
+  const trucksInfo = truck_weights.map((weight) => ({ location: bridge_length, weight }));
+  const bridge = [];
+  const done = [];
+  let count = 0;
+
+  while (trucksInfo.length || bridge.length) {
+    if (bridge.length && bridge[0].location === 0) {
+      done.push(bridge.shift());
+    }
+    if (trucksInfo.length && bridge.reduce((acc, cur) => acc + cur.weight, 0) + trucksInfo[0].weight <= weight) {
+      bridge.push(trucksInfo.shift());
+    }
+
+    for (const key in bridge) {
+      bridge[key].location--;
+    }
+    count++;
+  }
+
+  return count;
+}
