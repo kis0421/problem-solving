415923cf7a3d513541f9e43171f2a54ac9cfd5e7
Minkyu Kim <kig0421@gmail.com>
2022-02-09 16:40:15 +0900

    Update 92334_신고 결과 받기.js

--- 92334_신고 결과 받기.js
+++ 92334_신고 결과 받기.js
@@ -2,17 +2,13 @@ function solution(id_list, report, k) {
   const uniqueReport = Array.from(new Set(report));
   const ids = uniqueReport.reduce((acc, cur, index) => {
     const [_, target] = cur.split(" ");
-    acc[target] = acc[target] === undefined
-      ? 1
-      : acc[target] + 1;
+    acc[target] = (acc[target] || 0) + 1;
     return acc;
   }, {});
   const reportCount = uniqueReport.reduce((acc, cur, index) => {
     const [reporter, target] = cur.split(" ");
     if (ids[target] && ids[target] >= k) {
-      acc[reporter] = acc[reporter] === undefined
-        ? 1
-        : acc[reporter] + 1;
+      acc[reporter] = (acc[reporter] || 0) + 1;
     }
     return acc;
   }, {});
