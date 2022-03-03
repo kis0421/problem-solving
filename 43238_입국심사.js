2126f5de1396c5c93fc3059a3c38b9605ef1d90b
Minkyu Kim <kig0421@gmail.com>
2022-03-04 07:52:24 +0900

    Update 43238_입국심사.js

--- 43238_입국심사.js
+++ 43238_입국심사.js
@@ -1,9 +1,14 @@
 function solution(n, times) {
   function immigrationCheck(targetTime) {
-    const availableSupervisors = times.filter((i) => i <= targetTime);
-    const totalQuantityPerTime = availableSupervisors.reduce((acc, cur) => acc + (Math.floor(targetTime / cur)), 0);
+    const totalQuantityPerTime = times.reduce((acc, cur) => {
+      if (cur <= targetTime) {
+        acc += Math.floor(targetTime / cur);
+      }
+      return acc;
+    }, 0);
     return totalQuantityPerTime >= n;
   }
+  
   let timeLimit = 10e8;
   let maxTime = 0;
   while (maxTime !== timeLimit) {
