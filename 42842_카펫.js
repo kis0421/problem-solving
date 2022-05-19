44a2c6e1223e0aaeb31f98a2f4e1679e8801ec9d
Minkyu Kim <kig0421@gmail.com>
2022-05-19 20:04:28 +0900

    Update 42842_카펫.js
    refactor

--- 42842_카펫.js
+++ 42842_카펫.js
@@ -1,18 +1,13 @@
 function solution(brown, yellow) {
   const totalBlockCount = brown + yellow;
-  if (brown === yellow * 2) {
-    return [brown / (yellow / 2) * 2, yellow / 2];
-  }
   let brownLength = 1;
 
   while (true) {
     const expectedNumber = totalBlockCount / brownLength;
     if (Number.isInteger(expectedNumber) && expectedNumber > 1 && brownLength >= expectedNumber &&
       (brownLength - 2) * (expectedNumber - 2) === yellow) {
-      break;
-    } else {
-      brownLength++;
+      return [brownLength, expectedNumber];
     }
+    brownLength++;
   }
-  return [brownLength, totalBlockCount / brownLength];
 }
