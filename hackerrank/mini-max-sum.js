b3f960965d01584769f21d2cab49b3a1485417d8
Minkyu Kim <kig0421@gmail.com>
2022-10-17 00:03:58 +0900

    Update mini-max-sum.js

--- hackerrank/mini-max-sum.js
+++ hackerrank/mini-max-sum.js
@@ -1,6 +1,9 @@
 
 function miniMaxSum(arr) {
   const numbers = arr.slice().sort((a, b) => a - b);
-  return [...numbers.slice(1, 4), ...numbers.slice(1, 4), numbers[0], numbers[numbers.length - 1]]
+  const commonTotal = numbers
+    .slice(1, 4)
     .reduce((acc, cur) => acc + cur);
+
+  console.log(`${commonTotal + numbers[0]} ${commonTotal + numbers[numbers.length - 1]}`);
 }
