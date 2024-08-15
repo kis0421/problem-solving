7fe75b130e2b916520f435045bf70b702bfc36bd
Minkyu Kim <kig0421@gmail.com>
2024-08-15 15:03:27 +0900

    Create remove-duplicates-from-sorted-array.ts

---
+++ leetcode/easy/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,9 @@
+function removeDuplicates(nums: number[]): number {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i -= 1;
+    }
+  }
+  return nums.length;
+}
